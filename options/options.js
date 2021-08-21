db2ae1390efbcc4dffbbf31dca3c04600a8b5609
Crowdbotics <team@crowdbotics.com>
2021-08-21 10:49:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "test2_30006Identifier",
+  url: "https://test2_30006Identifier.botics.co",
+  api: "https://test2_30006Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
